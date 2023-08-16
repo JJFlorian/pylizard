c37bf9363eb23acda836d33c16e9dc2f92501338
Florian Knappers <73856313+JJFlorian@users.noreply.github.com>
2023-08-16 11:46:02 +0200

    monitoring network get methods DRY: defined function in utils

--- pylizard/utils.py
+++ pylizard/utils.py
@@ -4,8 +4,7 @@ from .config import *
 
 def get_organisation_endpoint_assets(endpoint, dataformat, organisation_uuid, json_field, headers):
     """
-    Function that handles all endpoint asset requests.
-    Generally used in the methods of pylizard classes that start with 'get_'
+    Function that handles the Organisation class get methods.
     """
     if endpoint == 'timeseries':
         next_url = f'{LIZARD_BASE_URL}/{endpoint}/?location__organisation__uuid={organisation_uuid}&limit=250'
@@ -27,6 +26,33 @@ def get_organisation_endpoint_assets(endpoint, dataformat, organisation_uuid, js
                 df_list.append(result)
 
         next_url = r.json()['next']
+
+    if dataformat == "list":
+        return uuid_list
+    elif dataformat == "df":
+        df = pd.DataFrame(df_list)
+        return df
+    
+def get_monitoringnetwork_enpoint_assets(endpoint, dataformat,url, headers):
+    """
+    Function that handles the monitoringnetwork class get methods.
+    """
+    uuid_list = []
+    df_list = []
+    next_url = f"{url}/{endpoint}/?limit=250"
+
+    while next_url != None:
+        r = requests.get(next_url, headers=headers)
+        results = r.json()["results"]
+
+        if dataformat == "list":
+            for result in results:
+                uuid_list.append(result["uuid"])
+        elif dataformat == "df":
+            for result in results:
+                df_list.append(result)
+
+        next_url = r.json()["next"]
 
     if dataformat == "list":
         return uuid_list
